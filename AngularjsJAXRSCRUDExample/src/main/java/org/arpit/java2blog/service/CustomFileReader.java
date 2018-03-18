b4681e4b20081d3bb79fc6352b82c5fec5bf30e3
Rakesh-Amaragomara <rockygoud43@gmail.com>
2018-03-18 17:31:20 +0530

    Add files via upload

--- AngularjsJAXRSCRUDExample/src/main/java/org/arpit/java2blog/service/CustomFileReader.java
+++ AngularjsJAXRSCRUDExample/src/main/java/org/arpit/java2blog/service/CustomFileReader.java
@@ -1,12 +1,11 @@
 package com.rakesh.home.test;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -27,8 +26,16 @@ private List<User> getFullListUser(User user,String file){
 	JSONParser parser = new JSONParser();
 	try (Reader is = new FileReader(fileObj)) {
 		JSONArray jsonArray = (JSONArray) parser.parse(is);
+		for(int i=0;i<jsonArray.size();i++){
+			JSONObject json=(JSONObject) jsonArray.get(i);
+			System.out.println((String)json.get("username"));
+		}
 		List<User> list=(List<User>) jsonArray.stream().filter(userObj->isCritiriaMatched((JSONObject) userObj,user)).collect(Collectors.toList());
 		return list;
+		/*Iterator<JSONObject> itr=jsonArray.iterator();
+		while(itr.hasNext()){
+			System.out.println(itr.next().get("username"));
+		}*/
 		} catch(Exception e) {
 		e.printStackTrace();
 	}
@@ -67,31 +74,67 @@ private Function<String,File> getFileObjFromJson=filename->{
 
 public static void main(String args[]){
 	CustomFileReader cust=new CustomFileReader();
-	User user = new User();
-	user.setUsername("");
-	user.setMail("rakesh.goud.amaragomara@intl.verizon.com");
-	user.setVerificationPin("123456");
-	List<User> users=cust.getFullListUser(user, "user.json");
-	if(users.size()==1){
-		System.out.println("username feature is adopted");
-		cust.ssotempcache.put(users.get(0).getUsername(), "usernameFeature is adopted");
-		System.out.println("success only one user found now check for authentication type if it is a mobile then lock the user and send a mail username already specified on the mobile device");
-	}else if(users.size()>1){
-		System.out.println("unlock all the users");
-	}else {
-		System.out.println("something hacking here");
-		if(cust.ssotempcache.get(user.getMail())!= null){
-			int x =(Integer)cust.ssotempcache.get(user.getMail())+1;
-			cust.ssotempcache.put(user.getMail(),x);
-			if(x > 3){
-				System.out.println("lock the user");
-			}
-		}else{
-			cust.ssotempcache.put(user.getMail(), 1);
-		}
-		
-	}
+	System.out.println(cust.wrieFileWithEntries());
+	System.out.println(cust.readFileWithEntries());
+	
 }
 
-
+private Function<String,File> getFileObject=filename->{
+	ClassLoader loader=getClass().getClassLoader();
+	File file= new File(loader.getResource(filename).getFile());
+	return file;
+};
+private boolean wrieFileWithEntries(){
+	File file=getFileObject.apply("mytest.properties");
+	OutputStream  obj = null;
+	try {
+		Properties prop=new Properties();
+		prop.setProperty("rakesh", "genius");
+		prop.setProperty("rishik", "brilliant");
+		obj=new FileOutputStream(file);
+		prop.store(obj, "this is true");
+			return true;
+	
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return false;
+	}finally{
+		if (obj != null) {
+			try {
+				obj.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+}}
+private boolean readFileWithEntries(){
+	File file=getFileObject.apply("mytest.properties");
+	FileReader obj = null;
+	try {
+		Properties prop=new Properties();
+		//prop.setProperty("rakesh", "genius");
+		obj=new FileReader(file);
+		prop.load(obj);
+		System.out.println(prop.getProperty("rakesh"));
+		System.out.println(prop.getProperty("rishik"));
+		//prop.store(obj, "this is true");
+		
+			return true;
+	
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return false;
+	}finally{
+		if (obj != null) {
+			try {
+				obj.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+}}
 }
